ec1c2669966ea79d59ecaae9ed64c3fc53645399
Yusuke Mukai <opposite0302@gmail.com>
2015-06-27 14:54:23 +0900

    Implemented multiple data source

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,3 +1,15 @@
 class Creditcard < ActiveRecord::Base
+
+  unless ENV['CLOUD_DATABASE_HOST'] == nil
+    establish_connection(
+      :adapter  => 'ibm_db',
+      :host     => ENV['CLOUD_DATABASE_HOST'],
+      :port     => ENV['CLOUD_DATABASE_PORT'],
+      :database => ENV['CLOUD_DATABASE_NAME'],
+      :username => ENV['CLOUD_DATABASE_USER'],
+      :password => ENV['CLOUD_DATABASE_PASS']
+    )
+  end
+
   belongs_to :customer
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,14 @@
 class Customer < ActiveRecord::Base
+
+  unless ENV['CLOUD_DATABASE_HOST'] == nil
+    establish_connection(
+      :adapter  => 'ibm_db',
+      :host     => ENV['CLOUD_DATABASE_HOST'],
+      :port     => ENV['CLOUD_DATABASE_PORT'],
+      :database => ENV['CLOUD_DATABASE_NAME'],
+      :username => ENV['CLOUD_DATABASE_USER'],
+      :password => ENV['CLOUD_DATABASE_PASS']
+    )
+  end
+
 end
