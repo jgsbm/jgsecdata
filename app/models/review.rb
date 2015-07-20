9d74f48a8ff32c7d263810a872d1048e5c52f1b6
Yusuke Mukai <yusuke@mac.local>
2015-07-21 01:35:46 +0900

    removed debug code

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ActiveRecord::Base
 
   after_commit do
     cache = BluemixDatacache::Client.new("JGS.NONE")
-    p product_reviews = self.class.where(:product_code => self.product_code).to_json
-    p cache.insert(self.product_code, product_reviews, 'application/json')
+    product_reviews = self.class.where(:product_code => self.product_code).to_json
+    cache.insert(self.product_code, product_reviews, 'application/json')
   end
 end
